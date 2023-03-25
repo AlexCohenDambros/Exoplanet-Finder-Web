ed7c032e7c523a83f130ad1fa6bed3388084438c
oMullerr <leindorfmuller@gmail.com>
2023-03-25 14:35:10 -0300

    add correct languages

--- UI/src/app/header/header.component.ts
+++ UI/src/app/header/header.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ThemeService } from '../switch-theme/theme.service';
+import { take } from 'rxjs';
+import { ThemeStateComponent } from '../switch-theme/theme-state/theme-state.component';
+import { LanguageService } from '../translate/language.service';
 
 @Component({
   selector: 'app-header',
@@ -8,39 +11,70 @@ import { ThemeService } from '../switch-theme/theme.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  themeState!: ThemeStateComponent
+  selectedOption!: number;
 
   constructor(
     public themeService: ThemeService,
+    public languageService: LanguageService,
     public router: Router,
-    ) { }
+    ) {
+      this.themeState = new ThemeStateComponent();
+     }
 
-  options = [
-    {label: 'Opção 1', value: 1},
-    {label: 'Opção 2', value: 2},
-    {label: 'Opção 3', value: 3},
-  ];
-
-  selectedOption!: number;
+     languages = [
+      {value: 1, label: 'Português', icon: ''},
+      {value: 2, label: 'Inglês', icon: ''},
+      {value: 3, label: 'Alemão', icon: ''},
+      {value: 4, label: 'Chinês', icon: ''},
+      {value: 5, label: 'Espanhol', icon: ''},
+      {value: 6, label: 'Francês', icon: ''},
+    ];
 
   public goHome(): void {
     this.router.navigate(['/home']);
   }
 
-  public onOptionSelected(): void {
-    console.log("Opção selecionada: " + this.selectedOption);
+  toggleTheme() {
+    this.themeService.getIsDarkMode()
+    .pipe(take(1))
+    .subscribe(isDarkMode => {
+      this.themeService.setIsDarkMode(!isDarkMode);
+    });
+  }
 
+  public changeLanguage(lang: string): void {
+    this.languageService.setLanguage(lang);
+  }
+
+  public onLanguageSelected(): void {
     switch(this.selectedOption) {
       case 1:
-        console.log("Opção 1 selecionada");
+        console.log("portugues selecionado");
+        this.changeLanguage('pt');
         break;
       case 2:
-        console.log("Opção 2 selecionada");
+        console.log("ingles selecionado");
+        this.changeLanguage('en-US');
         break;
       case 3:
-        console.log("Opção 3 selecionada");
+        console.log("alemao selecionado");
+        this.changeLanguage('al')
+        break;
+      case 4:
+        console.log("chines selecionado");
+        this.changeLanguage('ch')
+        break;
+      case 5:
+        console.log("espanhol selecionado");
+        this.changeLanguage('es')
+        break;
+      case 6:
+        console.log("frances selecionado");
+        this.changeLanguage('fr')
         break;
       default:
-        console.log("Opção inválida selecionada");
+        this.changeLanguage('pt');
         break;
     }
   }
