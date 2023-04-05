bbc1c961af0df57758dddbc11f2791065c20b057
oMullerr <leindorfmuller@gmail.com>
2023-04-05 16:22:47 -0300

    fixing the folders destination

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ServerErrorComponent } from './http-errors/server-error/server-error.component';
-import { NotFoundComponent } from './http-errors/not-found/not-found.component';
-import { HomeComponent } from './home/home.component';
-import { TestComponent } from './test/test.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './features/home/home.component';
+import { NotFoundComponent } from './features/http-errors/not-found/not-found.component';
+import { ServerErrorComponent } from './features/http-errors/server-error/server-error.component';
+import { TestComponent } from './shared/test/test.component';
+import { LoginComponent } from './features/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
--- UI/src/app/features/http-errors/not-found/not-found.component.ts
+++ UI/src/app/features/http-errors/not-found/not-found.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ThemeService } from '../../switch-theme/theme.service';
+import { ThemeService } from 'src/app/shared/switch-theme/theme.service';
 
 @Component({
   selector: 'app-not-found',
