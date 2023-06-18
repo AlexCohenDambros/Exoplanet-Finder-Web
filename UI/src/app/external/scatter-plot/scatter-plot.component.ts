b0df53750b58ae2d52861e3ddc84759813b94405
oMullerr <leindorfmuller@gmail.com>
2023-06-18 01:23:46 -0300

    add import csv and xlsx file and zoom graph

--- UI/src/app/external/scatter-plot/scatter-plot.component.ts
+++ UI/src/app/external/scatter-plot/scatter-plot.component.ts
@@ -9,54 +9,75 @@ import * as XLSX from 'xlsx';
 })
 export class ScatterPlotComponent {
 
-  selecionarArquivo(event: any) {
-    const arquivo = event.target.files[0];
-    console.log('Arquivo selecionado:', arquivo);
+  selectFile(event: any) {
+    const file = event.target.files[0];
+    console.log('Arquivo selecionado:', file);
 
-    if (arquivo) {
-      this.leDadosDoExcel(arquivo)
-        .then(dados => {
-          this.plotarGraficoDeDispersao(dados, 'colunaX', 'colunaY');
+    if (file) {
+      this.readExcel(file)
+        .then(data => {
+          this.plotScatterplot(data, 'flux', 'flux_err');
         })
         .catch(error => {
-          console.error('Erro ao ler o arquivo:', error);
+          console.error('Error reading file:', error);
         });
     }
   }
 
-  leDadosDoExcel(file: File): Promise<any[]> {
+  readExcel(file: File): Promise<any[]> {
     return new Promise<any[]>((resolve, reject) => {
       const reader: FileReader = new FileReader();
 
       reader.onload = (e: any) => {
-        const data = new Uint8Array(e.target.result);
-        const workbook = XLSX.read(data, { type: 'array' });
-        const sheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[sheetName];
-        const jsonData = XLSX.utils.sheet_to_json<any>(worksheet, { header: 1 });
+        const data = e.target.result;
+        const extension = file.name.split('.').pop()?.toLowerCase();
 
-        const header = Object.keys(jsonData[0]);
-        const dataRows = jsonData.slice(1);
-        const dados = dataRows.map(row => {
-          const item: any = {};
-          header.forEach((column, index) => {
-            item[column] = row[index];
+        if (extension === 'xlsx') {
+          const workbook = XLSX.read(data, { type: 'array' });
+          const sheetName = workbook.SheetNames[0];
+          const worksheet = workbook.Sheets[sheetName];
+          const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+          const header: string[] = Object.keys(jsonData[0] as object);
+          const dataRows = jsonData.slice(1) as any[][];
+          const dados = dataRows.map((row: any[]) => {
+            const item: any = {};
+            header.forEach((column: string, index: number) => {
+              item[column] = row[index];
+            });
+
+            return item;
           });
-          return item;
-        });
+          resolve(dados);
 
-        resolve(dados);
+        } else if (extension === 'csv') {
+          const lines = data.split('\n');
+          const header = lines[0].split(',');
+          const dataRows = lines.slice(1) as string[];
+          const dados = dataRows.map((row: string) => {
+            const values = row.split(',');
+            const item: any = {};
+            header.forEach((column: string, index: number) => {
+              item[column] = values[index];
+            });
+            return item;
+          });
+
+          resolve(dados);
+        } else {
+          reject('Unsupported file type.');
+        }
       };
 
       reader.onerror = (error) => {
         reject(error);
       };
 
-      reader.readAsArrayBuffer(file);
+      reader.readAsText(file);
     });
   }
 
-  plotarGraficoDeDispersao(dados: any[], xColumn: string, yColumn: string) {
+  plotScatterplot(dados: any[], xColumn: string, yColumn: string) {
     const canvas: HTMLCanvasElement = document.getElementById('myChart') as HTMLCanvasElement;
     const ctx = canvas.getContext('2d');
 
@@ -64,13 +85,13 @@ export class ScatterPlotComponent {
       const labels = dados.map(item => item[xColumn]);
       const data = dados.map(item => item[yColumn]);
 
-      new Chart(ctx, {
+      const chart = new Chart(ctx, {
         type: 'scatter',
         data: {
           labels: labels,
           datasets: [
             {
-              label: yColumn,
+              label: xColumn,
               data: data,
               showLine: false,
               borderColor: 'rgba(0, 123, 255, 0.8)',
@@ -81,10 +102,63 @@ export class ScatterPlotComponent {
         options: {
           responsive: true,
           maintainAspectRatio: false,
+          animation: {
+            duration: 1000, // Duração da animação em milissegundos
+            easing: 'easeOutQuart', // Easing function para controlar a transição
+          },
         },
       });
+
+      // Função para manipular o evento de scroll do mouse
+      function handleMouseWheel(event: WheelEvent) {
+        const zoomFactor = 0.1; // Fator de zoom
+
+        // Verifica se a tecla Ctrl ou Command está pressionada
+        const isCtrlOrCmdPressed = event.ctrlKey || event.metaKey;
+
+        if (isCtrlOrCmdPressed) {
+          event.preventDefault();
+
+          // Obtém as coordenadas do mouse no canvas
+          const rect = canvas.getBoundingClientRect();
+          const offsetX = event.clientX - rect.left;
+          const offsetY = event.clientY - rect.top;
+
+          // Calcula o fator de escala
+          const scale = event.deltaY < 0 ? 1 + zoomFactor : 1 - zoomFactor;
+
+          // Obtém os limites do gráfico
+          const { left, top, width, height } = chart.chartArea;
+
+          // Calcula as coordenadas do mouse em relação aos dados
+          const xData = chart.scales['x'].getValueForPixel(offsetX);
+          const yData = chart.scales['y'].getValueForPixel(offsetY);
+
+          // Verifica se xData e yData estão definidos
+          if (xData !== undefined && yData !== undefined) {
+            // Calcula os novos limites do gráfico
+            const newWidth = width * scale;
+            const newHeight = height * scale;
+            const newLeft = xData - (xData - left) * scale;
+            const newTop = yData - (yData - top) * scale;
+
+            // Define os novos limites do gráfico
+            chart.chartArea.left = newLeft;
+            chart.chartArea.top = newTop;
+            chart.chartArea.right = newLeft + newWidth;
+            chart.chartArea.bottom = newTop + newHeight;
+
+            // Atualiza o gráfico
+            chart.update();
+          }
+        }
+      }
+
+      // Adiciona o evento de scroll do mouse ao canvas
+      canvas.addEventListener('wheel', handleMouseWheel);
+
     } else {
-      console.error('Não foi possível obter o contexto do canvas.');
+      console.error('Could not get canvas context.');
     }
   }
 }
