40984c09e1059c34eb572f1e68072b3b8acf9228
oMullerr <matheus.l.muller@gmail.com>
2023-08-16 19:11:44 -0300

    fixing the project

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home-page/home.page';
-import { LoginComponent } from './features/login-page/login.page';
-import { JoinComponent } from './features/join-page/join.page';
 import { NotFoundComponent } from './shared/http-errors/not-found/not-found.component';
 import { ServerErrorComponent } from './shared/http-errors/server-error/server-error.component';
 import { TestComponent } from './external/test/test.component';
-import { ScatterPlotComponent } from './external/scatter-plot/scatter-plot.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -14,9 +11,6 @@ const routes: Routes = [
   { path: '404', component: NotFoundComponent },
   { path: '500', component: ServerErrorComponent },
   { path: 'test', component: TestComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'join', component: JoinComponent },
-  { path: 'graph', component: ScatterPlotComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
