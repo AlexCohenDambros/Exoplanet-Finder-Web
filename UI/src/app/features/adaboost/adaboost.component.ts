bef9d7b6cdd2cc798b45350155ddfa26420d0894
gabrielscholze-r <gabrielscholze24@gmail.com>
2023-10-04 21:31:32 -0300

    models initial page

---
+++ UI/src/app/features/adaboost/adaboost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adaboost',
+  templateUrl: './adaboost.component.html',
+  styleUrls: ['./adaboost.component.scss']
+})
+export class AdaboostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ UI/src/app/features/xgboost/xgboost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-xgboost',
+  templateUrl: './xgboost.component.html',
+  styleUrls: ['./xgboost.component.scss']
+})
+export class XgboostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
