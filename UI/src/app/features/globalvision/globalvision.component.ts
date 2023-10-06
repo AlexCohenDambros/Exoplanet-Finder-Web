83fd9af6d33f98f66d71ac70803001b7a1485ced
gabrielscholze-r <gabrielscholze24@gmail.com>
2023-10-05 21:06:31 -0300

    visions👀

---
+++ UI/src/app/features/globalvision/globalvision.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-globalvision',
+  templateUrl: './globalvision.component.html',
+  styleUrls: ['./globalvision.component.scss']
+})
+export class GlobalvisionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
